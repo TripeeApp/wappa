08b49848c40955e535c1cb94fd3dc5a3bafe5555
rafaeldias <rafaeldias.c@gmail.com>
2019-08-20 16:32:42 -0300

    Added client.

---
+++ client.go
@@ -0,0 +1,98 @@
+package wappa
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	// Error message format.
+	errFmt = `Error while request the LigueTaxi API: %s; Status Code: %d; Body: %s.`
+)
+
+// requester is the interface that performs a request
+// to the server and parses the payload.
+type requester interface{
+	Request(ctx context.Context, method string, path string, body, output interface{}) error
+}
+
+// ApiError implements the error interface
+// and returns infos from the request
+type ApiError struct {
+	statusCode int
+	body       []byte
+	msg        string
+}
+
+func (e *ApiError) Error() string {
+	return fmt.Sprintf(errFmt, e.msg, e.statusCode, e.body)
+}
+
+
+// Client is responsible for handling requests to the Wappa API.
+type Client struct {
+	// client to comunicate with the API.
+	client *http.Client
+
+	// Host used for API requets.
+	// host should always be specified with a trailing slash.
+	host *url.URL
+}
+
+// Client returns a new Wappa API client with provided host URL and HTTP client.
+func New(host *url.URL, client *http.Client) *Client {
+	if client == nil {
+		client = &http.Client{}
+	}
+	return &Client{client, host}
+}
+
+// Request created an API request. A relative path can be providaded
+// in which case it is resolved relative to the host of the Client.
+func (c *Client) Request(ctx context.Context, method string, path string, body, output interface{}) error {
+	u, err := c.host.Parse(fmt.Sprintf("api/%s", path))
+	if err != nil {
+		return err
+	}
+
+	var b io.ReadWriter
+	if body != nil {
+		b = new(bytes.Buffer)
+		if err := json.NewEncoder(b).Encode(body); err != nil {
+			return err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), b)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	req = req.WithContext(ctx)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	// TODO: add tests for error on reading body
+	r, _ := ioutil.ReadAll(res.Body)
+
+	if err := json.Unmarshal(r, output); err != nil {
+		return &ApiError{
+			statusCode: res.StatusCode,
+			body:       r,
+			msg:        err.Error(),
+		}
+	}
+
+	return nil
+}
---
+++ client_test.go
@@ -0,0 +1,251 @@
+package wappa
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []struct{
+		host	*url.URL
+		client	*http.Client
+		wantClient *http.Client
+	}{
+		{&url.URL{}, nil, &http.Client{}},
+		{&url.URL{}, &http.Client{}, &http.Client{}},
+	}
+
+	for _, tc := range testCases {
+		c := New(tc.host, tc.client)
+
+		if c.host != tc.host {
+			t.Errorf("got c.host : %s; want %s.", c.host, tc.host)
+		}
+
+		if !reflect.DeepEqual(c.client, tc.wantClient) {
+			t.Errorf("got Client %+v; want %+v.", c.client, tc.wantClient)
+		}
+	}
+}
+
+func newMockServer(handler func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(handler))
+}
+
+type dummy struct {
+	Name string `json:"name"`
+}
+
+func TestClientRequest(t *testing.T) {
+	emptyObj := []byte(`{}`)
+
+	testCases := []struct{
+		endpoint string
+		method	 string
+		body	 interface{}
+		server	 *httptest.Server
+		wantOut	 interface{}
+	}{
+		{
+			"",
+			http.MethodGet,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				if r.Method != http.MethodGet {
+					t.Errorf("go Request.Method %s; want %s.", r.Method, http.MethodGet)
+				}
+				w.Write(emptyObj)
+			}),
+			dummy{},
+		},
+		{
+			"foo",
+			http.MethodGet,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				if want := "/api/foo"; r.URL.Path != want {
+					t.Errorf("got Request.URL: %s; want %s.", r.URL.Path, want)
+				}
+				w.Write(emptyObj)
+			}),
+			dummy{},
+		},
+		{
+			"foo",
+			http.MethodPost,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				if c := r.Header.Get("Content-Type"); c != "application/json" {
+					t.Errorf("got 'Content-Type' Header: '%s'; want 'application/json'.", c)
+				}
+				w.Write(emptyObj)
+			}),
+			dummy{},
+		},
+		{
+			"",
+			http.MethodGet,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				if r.Body != http.NoBody {
+					t.Errorf("got Request.Body: %+v, want empty.", r.Body)
+				}
+				w.Write(emptyObj)
+			}),
+			dummy{},
+		},
+		{
+			"",
+			http.MethodPost,
+			dummy{"Testing"},
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				if r.Body == http.NoBody {
+					t.Error("got Request.Body empty, want not empty.")
+				}
+
+				got, _ := ioutil.ReadAll(r.Body)
+
+				if want := []byte(`{"name":"Testing"}`); !bytes.Contains(got, want) {
+					t.Errorf("got body: %s, want %s.", got, want)
+				}
+				w.Write([]byte(`{"name":"Testing"}`))
+			}),
+			dummy{"Testing"},
+		},
+	}
+
+	for _, tc := range testCases {
+		var output dummy
+
+		u, _ := url.Parse(tc.server.URL)
+		c := New(u, nil)
+
+		err := c.Request(context.Background(), tc.method, tc.endpoint, tc.body, &output)
+		if err != nil {
+			t.Fatalf("got error calling Client.Request(context.Background(), %s, %s, %+v, %+v): %s; want nil.",
+				tc.method, tc.endpoint, tc.body, output, err.Error())
+		}
+
+		if !reflect.DeepEqual(output, tc.wantOut) {
+			t.Errorf("got output from Client.Request(): %+v; want %+v.", output, tc.wantOut)
+		}
+
+		tc.server.Close()
+	}
+}
+
+func TestClientRequestError(t *testing.T) {
+	testCases := []struct{
+		path		string
+		method		string
+		body		interface{}
+		server		*httptest.Server
+		assertError	func(e error)
+	}{
+		{
+			":",
+			http.MethodGet,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {}),
+			nil,
+		},
+		{
+			"",
+			http.MethodGet,
+			make(chan int),
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {}),
+			nil,
+		},
+		{
+			"",
+			",",
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {}),
+			nil,
+		},
+		{
+			"",
+			http.MethodPost,
+			nil,
+			httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})),
+			nil,
+		},
+		{
+			"",
+			http.MethodPost,
+			nil,
+			newMockServer(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+			}),
+			func(e error) {
+				err, ok := e.(*ApiError)
+				if !ok {
+					t.Fatal("got error different from *ApiError")
+				}
+
+				if err != nil {
+					if wantStatus := http.StatusInternalServerError; err.statusCode != wantStatus {
+						t.Errorf("got Error.statusCode: %d; want %d.", err.statusCode, wantStatus)
+					}
+
+					if wantBody := []byte(http.StatusText(http.StatusInternalServerError)); !bytes.Equal(wantBody, err.body) {
+						t.Errorf("got Error.body: %s; want %s.", err.body, wantBody)
+					}
+
+					if wantSubStr := "invalid character 'I'"; !strings.Contains(err.msg, wantSubStr) {
+						t.Errorf("go Error.msg: %s; want it to contain `%s` substring.", err.msg, wantSubStr)
+					}
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		u, _ := url.Parse(tc.server.URL)
+		c := New(u, nil)
+
+		err := c.Request(context.Background(), tc.method, tc.path, tc.body, &dummy{})
+		if err == nil {
+			t.Errorf("got error nil; want not nil.")
+		}
+
+		if tc.assertError != nil {
+			tc.assertError(err)
+		}
+
+		tc.server.Close()
+	}
+}
+
+func TestClientRequestWithContext(t *testing.T) {
+	s := newMockServer(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		select {
+		case <-time.After(1 * time.Second):
+			t.Errorf("Expected request to be canceled by context")
+		case <-ctx.Done():
+			return
+		}
+	})
+	defer s.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	u, _ := url.Parse(s.URL)
+	c := New(u, nil)
+
+	if err := c.Request(ctx, http.MethodGet, "/", nil, nil); err == nil {
+		t.Errorf("got error nil; want not nil")
+	}
+}
