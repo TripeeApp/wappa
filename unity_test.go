25edc27d892af69342abc47820c0681a9fdac3c3
rafaeldias <rafaeldias.c@gmail.com>
2019-08-21 19:42:27 -0300

    Updated tests with new helper functions.

--- unity_test.go
+++ unity_test.go
@@ -5,46 +5,11 @@ import (
 	"errors"
 	"net/http"
 	"net/url"
-	"reflect"
 	"testing"
 )
 
-type testRequester struct{
-	body	interface{}
-	ctx	context.Context
-	err	error
-	method	string
-	output	reflect.Value
-	path	endpoint
-
-}
-
-func (t *testRequester) Request(ctx context.Context, method string, path endpoint, body, output interface{}) error {
-	t.ctx = ctx
-	t.method = method
-	t.path = path
-	t.body = body
-
-	if t.output.IsValid() {
-		out := reflect.ValueOf(output)
-		if !out.IsNil() && out.Elem().CanSet() {
-			out.Elem().Set(t.output)
-		}
-	}
-
-	return t.err
-}
-
 func TestUnity(t *testing.T) {
-	testCases := []struct{
-		name	string
-		call	func(ctx context.Context, req requester) (resp interface{}, err error)
-		ctx	context.Context
-		method	string
-		path	endpoint
-		body	interface{}
-		wantRes	interface{}
-	}{
+	testCases := []testTable{
 		{
 			"Read()",
 			func(ctx context.Context, req requester) (resp interface{}, err error) {
@@ -107,44 +72,12 @@ func TestUnity(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tc := tc // creates scoped test case
-		t.Run(tc.name, func(t *testing.T) {
-			req := &testRequester{output: reflect.ValueOf(tc.wantRes).Elem()}
-
-			res, err := tc.call(tc.ctx, req)
-			if err != nil {
-				t.Fatalf("got error while calling User %s: %s, want nil", tc.name, err.Error())
-			}
-
-			if !reflect.DeepEqual(req.ctx, tc.ctx) {
-				t.Errorf("got Requester Context %+v; want %+v.", req.ctx, tc.ctx)
-			}
-
-			if req.method != tc.method {
-				t.Errorf("got request method: %s; want %s.", req.method, tc.method)
-			}
-
-			if req.path != tc.path {
-				t.Errorf("got request path: %s; want %s.", req.path, tc.path)
-			}
-
-			if !reflect.DeepEqual(req.body, tc.body) {
-				t.Errorf("got request body: %+v; want %+v.", req.body, tc.body)
-			}
-
-			if !reflect.DeepEqual(res, tc.wantRes) {
-				t.Errorf("got response: %+v; want %+v.", res, tc.wantRes)
-			}
-		})
+		t.Run(tc.name, test(tc))
 	}
 }
 
 func TestUnityError(t *testing.T) {
-	testCases := []struct{
-		name	string
-		call	func(req requester) error
-		err	error
-	}{
+	testCases := []testTableError{
 		{
 			"Read()",
 			func(req requester) error {
@@ -180,14 +113,6 @@ func TestUnityError(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tc := tc // creates scoped test case
-		t.Run(tc.name, func(t *testing.T) {
-			req := &testRequester{err: tc.err}
-
-			err := tc.call(req)
-			if !reflect.DeepEqual(err, tc.err) {
-				t.Errorf("got error: %s; want %s.", err, tc.err)
-			}
-		})
+		t.Run(tc.name, testError(tc))
 	}
 }
