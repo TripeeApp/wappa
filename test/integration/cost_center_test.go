30d269e487dc56b41c8a248e78ce4082cde0393d
rafaeldias <rafaeldias.c@gmail.com>
2019-08-23 17:59:28 -0300

    Added integration tests for cost center.

---
+++ test/integration/cost_center_test.go
@@ -0,0 +1,62 @@
+package integration
+
+import(
+	"context"
+	"testing"
+
+	"bitbucket.org/mobilitee/wappa"
+)
+
+func TestCostCenter(t *testing.T) {
+	code := randString(5, letterBytes)
+	res, err := wpp.CostCenter.Create(context.Background(), &wappa.CostCenter{Name: "test", Code: code, CNPJ: "27906045000186"})
+	if err != nil {
+		t.Fatalf("got error while calling CostCenter.Read(): %s; want nil.", err.Error())
+	}
+
+	if !res.Success {
+		t.Fatalf("got error while creating a cost center: %s; want nil.", res.Message)
+	}
+
+	f := wappa.Filter{"code": code}
+	c, err := wpp.CostCenter.Read(context.Background(), f)
+	if err != nil {
+		t.Fatalf("got error while calling CostCenter.Read(%+v): %s; want nil.", f, err.Error())
+	}
+
+	if !c.Success {
+		t.Fatalf("got error while reading a cost center: %s; want nil.", res.Message)
+	}
+
+	costCenter := c.Response[0]
+
+	newCostCenter := &wappa.CostCenter{ID: costCenter.ID, Code: randString(5, letterBytes)}
+
+	opres, err := wpp.CostCenter.Update(context.Background(), newCostCenter)
+	if err != nil {
+		t.Fatalf("got error while calling CostCenter.Update(%+v): %s; want nil.", newCostCenter, err.Error())
+	}
+
+	if !opres.Success {
+		t.Fatalf("got error while updating a cost center: %s; want nil.", opres.Message)
+	}
+
+	f = wappa.Filter{"code": newCostCenter.Code}
+	c, err = wpp.CostCenter.Read(context.Background(), f)
+	if err != nil {
+		t.Fatalf("got error while calling CostCenter.Read(%+v): %s; want nil.", f, err.Error())
+	}
+
+	if !c.Success {
+		t.Fatalf("got error while reading a cost center: %s; want nil.", res.Message)
+	}
+
+	opres, err = wpp.CostCenter.Inactivate(context.Background(), costCenter.ID)
+	if err != nil {
+		t.Fatalf("got error while calling CostCenter.Inactivate(%+v): %s; want nil.", f, err.Error())
+	}
+
+	if !opres.Success {
+		t.Fatalf("got error while inactivating a cost center: %s; want nil.", opres.Message)
+	}
+}
