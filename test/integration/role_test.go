2c05c1cdf9bc7c4d625b0db7d3c4e3e73fb36a8a
rafaeldias <rafaeldias.c@gmail.com>
2019-08-23 18:07:09 -0300

    Added integration tests for roles.

--- test/integration/role_test.go
+++ test/integration/role_test.go
@@ -8,23 +8,23 @@ import(
 )
 
 func TestRole(t *testing.T) {
-	desc := randString(5, letterBytes)
-	res, err := wpp.Role.Create(context.Background(), &wappa.Role{ID: 123, Description: desc})
+	desc := randString(5, numberBytes)
+	res, err := wpp.Role.Create(context.Background(), &wappa.Role{Description: desc})
 	if err != nil {
 		t.Fatalf("got error while calling Role.Read(): %s; want nil.", err.Error())
 	}
 
 	if !res.Success {
-		t.Errorf("got error while creating a role: %s; want nil.", res.Message)
+		t.Fatalf("got error while creating an role: %s; want nil.", res.Message)
 	}
 
 	f := wappa.Filter{"desc": desc}
-	role, err := wpp.Role.Read(context.Background(), f)
+	r, err := wpp.Role.Read(context.Background(), f)
 	if err != nil {
 		t.Fatalf("got error while calling Role.Read(%+v): %s; want nil.", f, err.Error())
 	}
 
-	if !role.Success {
-		t.Errorf("got error while reading a role: %s; want nil.", res.Message)
+	if !r.Success {
+		t.Fatalf("got error while reading an role: %s; want nil.", res.Message)
 	}
 }
