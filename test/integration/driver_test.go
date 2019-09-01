8b1394a16e3f97f3ff1304cd6ed9dd65249feb7c
rdleal <rafael.leal@mobilitee.com.br>
2019-08-31 22:26:59 -0300

    Added integration test for Driver service.

---
+++ test/integration/driver_test.go
@@ -0,0 +1,41 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/rdleal/wappa"
+)
+
+func TestDriver(t *testing.T) {
+	if !checkAuth("TestDriver") {
+		return
+	}
+
+	empID, ok := findEmployeeID(employeeFilter)
+	if !ok {
+		return
+	}
+
+	f := wappa.Filter{
+		"lat": []string{fmt.Sprintf("%.7f", latOrigin)},
+		"lng": []string{fmt.Sprintf("%.7f", lngOrigin)},
+		"type": []string{"1"}, // TODO: Get from Env and defaults to 6 (Carro Particular)
+		"employee": []string{strconv.Itoa(empID)},
+	}
+
+	d, err := wpp.Driver.Nearby(context.Background(), f)
+	if err != nil {
+		t.Fatalf("got error while calling Driver.Nearby(%+v): %s; want nil.", f, err.Error())
+	}
+
+	if !d.Success {
+		t.Errorf("got failed response while reading nearby drivers: '%s'; want it to be successful.", d.Message)
+	}
+
+	if len(d.Drivers) == 0 {
+		fmt.Printf("no drivers for location (%.7f, %.7f).", latOrigin, lngOrigin)
+	}
+}
