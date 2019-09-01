4a216d5747cba82bb7e7b4f88a63a9b35e67e379
rdleal <rafael.leal@mobilitee.com.br>
2019-09-01 13:32:41 -0300

    Updated custom time type.

---
+++ time.go
@@ -0,0 +1,33 @@
+package wappa
+
+import  "time"
+
+const timeLayout = `2006-01-02T15:04:05`
+
+// Time is a custom time type for
+// unmashaling data from the API.
+type Time struct {
+	time.Time
+}
+
+func (t *Time) UnmarshalJSON(b []byte) error {
+	var err error
+
+	s := string(b)
+	if s == "null" {
+		return err
+	}
+	t.Time, err = time.Parse(`"`+timeLayout+`"`, s)
+	return err
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	if t.IsZero() {
+		return []byte("null"), nil
+	}
+	b := make([]byte, 0, len(timeLayout))
+	b = append(b, '"')
+	b = t.AppendFormat(b, timeLayout)
+	b = append(b, '"')
+	return  b, nil
+}
